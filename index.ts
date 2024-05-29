eb617f5e7fbcc74f2338f7e606fdc5446b4f275c
moeenaalam <moeenaalam@hotmail.com>
2024-05-29 16:01:51 +0500

    first upload

---
+++ index.ts
@@ -0,0 +1,55 @@
+#! usr/bin/env node
+
+
+
+import inquirer from "inquirer";
+
+//get user input
+let userInput = await inquirer.prompt([
+
+    {
+        name: "num1",
+        type: "number",
+        message: "Enter your first number: " // first number input
+    },
+    {
+        name: "num2",
+        type: "number",
+        message: "Enter your second number: " //second number input
+    },
+    {
+        name: "operation",
+        type: "list",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"] //opertion to be performed
+    }
+
+
+])
+
+//create a function which takes 2 number and 1 string as arguments and return resulting number
+function calculate(num1:number, num2:number, operation:string){
+
+    switch (operation) {
+        case "Addition":
+            return num1+num2;
+
+        case "Subtraction":
+            return num1-num2;
+
+        case "Multiplication":
+            return num1*num2;
+
+        case "Division":
+            return num1/num2;
+            
+        default:
+            break;
+    }
+
+}
+
+let result = calculate(userInput.num1, userInput.num2, userInput.operation); //call the function and store result in a variable
+
+
+console.log(`Answer is ${result}.`); // print result
+
